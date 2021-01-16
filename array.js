3e593818339e7bdb69c8baf33064a158f4dbd05f
stsharin <stssharin@gmail.com>
2021-01-17 01:35:04 +0600

    first update

---
+++ array.js
@@ -0,0 +1,34 @@
+var friendsAge = [15, 14, 17, 19];
+console.log("Array: "+ friendsAge); 
+
+// position
+console.log("0th position: " + friendsAge[0]); 
+
+var sonaliAge = friendsAge[2];
+console.log("Sonali's age: " + sonaliAge);
+
+// update
+friendsAge[1] = 21;
+console.log("Updated array: " + friendsAge);
+
+// position
+var position = friendsAge.indexOf(19);
+console.log("Position of 19: " + position);
+
+// length
+console.log("Array length: " + friendsAge.length);
+
+// push (adds elements to the end)
+friendsAge.push(16);
+friendsAge.push(25);
+console.log(friendsAge);
+console.log("New Array length: " + friendsAge.length);
+
+// pop (the last element)
+friendsAge.pop();
+console.log("New Array: " + friendsAge);
+console.log("New Array length: " + friendsAge.length);
+
+// unshift (adds element to the beginning)
+friendsAge.unshift(30);
+console.log(friendsAge);
